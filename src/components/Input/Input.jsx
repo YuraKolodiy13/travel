eb4c84cecf8678114a7d2d3ccf64c9cd3dc4199e
YuraKolodiy13 <timber2017@gmail.com>
2022-07-05 16:53:24 +0200

    continue testing

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -4,7 +4,12 @@ import './Input.scss';
 const Input = ({value, onChange}) => {
   return (
     <label>
-      <input className={value.length ? 'filled' : ''} type="text" value={value} onChange={onChange}/>
+      <input
+        className={value.length ? 'filled' : ''}
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
     </label>
   )
 }
