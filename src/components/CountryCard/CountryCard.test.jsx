85966fc25729bbf236e37ae201141ca11743670f
YuraKolodiy13 <timber2017@gmail.com>
2022-07-21 13:41:29 +0200

    fix test error

--- src/components/CountryCard/CountryCard.test.jsx
+++ src/components/CountryCard/CountryCard.test.jsx
@@ -1,21 +1,22 @@
 import {render, fireEvent} from '@testing-library/react';
 import CountryCard from "./CountryCard";
 import React from "react";
+import {MemoryRouter} from "react-router";
 
 describe('testing CountryCard component', () => {
 
   test('testing render', () => {
-    const {getByRole} = render(<CountryCard name='test' imgSrc='test'/>)
+    const {getByRole} = render(<CountryCard name='test' imgSrc='test'/>, {wrapper: MemoryRouter})
     expect(getByRole('listitem')).toBeInTheDocument();
   })
 
   test('testing render without props', () => {
-    const {queryByRole} = render(<CountryCard/>)
+    const {queryByRole} = render(<CountryCard/>, {wrapper: MemoryRouter})
     expect(queryByRole('listitem')).toBeNull();
   })
 
   test('testing img onError func', () => {
-    const {getByRole} = render(<CountryCard name='test' imgSrc='test'/>);
+    const {getByRole} = render(<CountryCard name='test' imgSrc='test'/>, {wrapper: MemoryRouter});
     fireEvent.error(getByRole('img'));
     expect(getByRole('img')).toHaveAttribute('src', '/no-avatar.jpg')
   })
